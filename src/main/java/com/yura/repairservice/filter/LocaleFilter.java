15283bf7521062fc591db68c7c10c66451322dad
Yura <yuriy.petrashenko7@gmail.com>
2020-02-04 17:23:19 +0200

    change Readme

--- src/main/java/com/yura/repairservice/filter/LocaleFilter.java
+++ src/main/java/com/yura/repairservice/filter/LocaleFilter.java
@@ -1,7 +1,5 @@
 package com.yura.repairservice.filter;
 
-import sun.awt.X11.XQueryTree;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;

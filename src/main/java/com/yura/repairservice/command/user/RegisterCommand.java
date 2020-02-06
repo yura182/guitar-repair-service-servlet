c2a8a7591ad9bf6dbcece4b3ffdf7639516bab40
Yura <yuriy.petrashenko7@gmail.com>
2020-02-06 12:50:31 +0200

    refactor exception handling

--- src/main/java/com/yura/repairservice/command/user/RegisterCommand.java
+++ src/main/java/com/yura/repairservice/command/user/RegisterCommand.java
@@ -3,11 +3,17 @@ package com.yura.repairservice.command.user;
 import com.yura.repairservice.command.Command;
 import com.yura.repairservice.domain.user.Role;
 import com.yura.repairservice.domain.user.User;
+import com.yura.repairservice.exception.AlreadyRegisteredUserException;
+import com.yura.repairservice.exception.InvalidUserParameterException;
 import com.yura.repairservice.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class RegisterCommand implements Command {
+    private static final Logger LOGGER = LogManager.getLogger(RegisterCommand.class);
+
     private final UserService userService;
 
     public RegisterCommand(UserService userService) {
@@ -25,7 +31,17 @@ public class RegisterCommand implements Command {
                 .withRole(Role.CLIENT)
                 .build();
 
-        userService.register(user);
+        try {
+            userService.register(user);
+        } catch (InvalidUserParameterException e) {
+            LOGGER.warn("Validation error " + e);
+            request.setAttribute("errorMessage", "register.error");
+            return "register.jsp";
+        } catch (AlreadyRegisteredUserException e) {
+            LOGGER.warn("User with such email already exist " + e);
+            request.setAttribute("errorMessage", "register.error.already.registered");
+            return "register.jsp";
+        }
 
         request.getSession().setAttribute("successMessage", "login.just.registered");
 
--- src/main/java/com/yura/repairservice/exception/AlreadyRegisteredUserException.java
+++ src/main/java/com/yura/repairservice/exception/AlreadyRegisteredUserException.java
@@ -3,7 +3,6 @@ package com.yura.repairservice.exception;
 public class AlreadyRegisteredUserException extends RuntimeException {
 
     public AlreadyRegisteredUserException() {
-
     }
 
     public AlreadyRegisteredUserException(String message) {
